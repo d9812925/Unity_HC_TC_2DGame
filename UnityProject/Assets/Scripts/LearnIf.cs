d0e4e73be3b9a2acbf457215e8d1d2869b4745cd
d9812925 <58072893+d9812925@users.noreply.github.com>
2019-11-22 21:49:07 +0800

    認識C#基礎

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public int combo;
+    private void Start()
+    {
+        if (combo >= 50 && combo<100)
+        {
+        print("dmgx2");
+        }
+        else if(combo >= 100 && combo <150)
+        {
+        print("dmgx5")
+        }
+        else if(combo >=150 && combo < 200)
+        {
+        print("dmgx10")
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Learnoperater.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+
+public class Learnoperater : MonoBehaviour
+{
+
+
+    public int a = 10, b = 3;
+    public int c = 25, d = 15;
+    public bool boolA = true, boolB = false;
+    public int Hp = 100, item = 10;
+
+    private void Start()
+    {
+        #region
+        print(a + b); 
+        print(a - b);
+        print(a * b);
+        print(a / b);
+        print(a % b);
+        #endregion
+
+        #region
+        print(c<d);
+        print(c>d);
+        print(c<=d);
+        print(c<=d);
+        print(c==d);
+        print(c!=d);
+        #endregion
+
+        #region
+        print(true && true);
+        print(true && false);
+        print(false && true);
+        print(false && false);
+
+        print(true || true);
+        print(true || false);
+        print(false || true);
+        print(false || false);
+
+        print(!true);
+        print(!false);
+
+        print(Hp > 50 && item > 7);
+        #endregion
+    }
+
+
+}
